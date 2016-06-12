23149a1e59b75fd3f7c35f6da20eb696b9fd4e82
Csini <csini@gmx.com>
2016-06-12 22:08:34 +0200

    node_modules gitignored

--- src/node_modules/popsicle/dist/browser/tough-cookie.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var CookieJar = (function () {
-    function CookieJar() {
-        throw new TypeError('Cookie jars are only available on node');
-    }
-    return CookieJar;
-}());
-exports.CookieJar = CookieJar;
-//# sourceMappingURL=tough-cookie.js.map
--- src/node_modules/popsicle/dist/plugins/index.d.ts
+++
@@ -1,8 +0,0 @@
-import Promise = require('any-promise');
-export * from './common';
-import Request, { Middleware } from '../request';
-import Response from '../response';
-export declare const unzip: () => (request: Request, next: () => Promise<Response>) => Promise<Response>;
-export declare function concatStream(encoding: string): (request: Request, next: () => Promise<Response>) => Promise<{}>;
-export declare function headers(): (request: Request, next: () => Promise<Response>) => Promise<Response>;
-export declare const defaults: Middleware[];
