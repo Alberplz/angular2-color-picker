d7e79c6a70849c0a9cea21a8dcafe1c7457b202a
Pedro Mello <pedro.mello@macquarie.com>
2017-03-30 14:04:18 +1100

    angular 4 update

--- lib/color-picker.module.ts
+++ lib/color-picker.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {NgModule, ModuleWithProviders} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {ColorPickerService} from './color-picker.service';
@@ -6,8 +6,15 @@ import {ColorPickerDirective} from './color-picker.directive';
 
 @NgModule({
     imports: [CommonModule],
-    providers: [ColorPickerService],
+    providers: [],
     declarations: [ColorPickerDirective],
     exports: [ColorPickerDirective]
 })
-export class ColorPickerModule {}
+export class ColorPickerModule {
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: ColorPickerModule,
+            providers: [ColorPickerService]
+        };
+    }
+}
